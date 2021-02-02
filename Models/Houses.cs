f7b0017b8438f8557356da8feeb1ce410634a7e7
Yayan Yanuari <yayanyanuari04@gmail.com>
2021-02-02 20:03:05 +0700

    First commit

---
+++ Models/Houses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sample.BingMaps.Models
+{
+    public class Houses
+    {
+        [Key]
+        public int IDHouse { get; set; }
+
+        public string LabelHouse { get; set; }
+
+        [Required]
+        public int TypeHouse { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+    }
+}
