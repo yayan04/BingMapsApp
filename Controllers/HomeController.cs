c995d1c5b897aa84cf1bdecc76e490d4b30792cb
Yayan Yanuari <yayanyanuari04@gmail.com>
2021-02-02 22:52:17 +0700

    tambah DBContext dan View Create

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sample.BingMaps.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace Sample.BingMaps.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDBContext _db = new ApplicationDBContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Houses.ToList()); ;
         }
 
         public ActionResult About()
@@ -25,6 +28,31 @@ namespace Sample.BingMaps.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                var hs = new Houses
+                {
+                    LabelHouse = collection["LabelHouse"].ToString(),
+                    TypeHouse = Convert.ToInt32(collection["TypeHouse"].ToString()),
+                    Address = collection["Address"].ToString()
+
+                };
+                _db.Houses.Add(hs);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(collection);
         }
     }
 }
---
+++ Models/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Sample.BingMaps.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext() : base ("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Houses> Houses { get; set; }
+    }
+}
